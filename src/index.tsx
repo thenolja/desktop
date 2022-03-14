f347d849dde382024ce629c39a03e7cacfbf6e52
parksaneon <sangeon1500@daum.net>
2022-03-14 16:50:11 +0900

    config : build enviromnent

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+import App from 'app/app';
+import store from './contexts/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
