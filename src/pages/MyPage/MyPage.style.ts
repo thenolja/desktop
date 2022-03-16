b9cccc2bbfecf75945ddf01b75232089ebbe6de0
hann3 <soyo_0131@naver.com>
2022-03-16 18:01:11 +0900

    💄 Styles: Signout Button Component

--- src/pages/MyPage/MyPage.style.ts
+++ src/pages/MyPage/MyPage.style.ts
@@ -1,20 +1,16 @@
 import styled from 'styled-components';
 
-const MyPageContainer = styled.section`
+const SignOut = styled.button`
   position: relative;
+  left: calc(100% - 120px);
+  width: 120px;
+  height: 48px;
+  border-radius: 10px;
   margin-bottom: 50px;
-  button {
-    position: absolute;
-    right: 0;
-    bottom: 10px;
-    width: 120px;
-    height: 48px;
-    border-radius: 10px;
-    font-weight: 600;
-    font-size: 20px;
-    color: #727483;
-    background-color: #f2f2f2;
-  }
+  font-weight: 600;
+  font-size: 20px;
+  color: #727483;
+  background-color: #f2f2f2;
 `;
 
-export default MyPageContainer;
+export default SignOut;
--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,15 +1,13 @@
 import ReservationList from 'components/Reservation/Reservation';
 import UserInfo from 'components/UserInfo/UserInfo';
-import MyPageContainer from './MyPage.style';
+import SignOut from './MyPage.style';
 
 const MyPage = () => {
   return (
     <>
       <UserInfo />
       <ReservationList />
-      <MyPageContainer>
-        <button>회원탈퇴</button>
-      </MyPageContainer>
+      <SignOut>회원탈퇴</SignOut>
     </>
   );
 };
