2c0ca914b46591bea582c2488102a192c7381e12
hann3 <soyo_0131@naver.com>
2022-03-16 15:35:56 +0900

    ♻️ Refactor: Separate Component and Page
    - 컴포넌트 폴더와 페이지 폴더를 분리

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,17 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import Header from 'components/header/Header';
 import Main from 'components/main/Main';
-import MyPage from 'components/myPage/MyPage';
+import MyPage from 'src/pages/MyPage/MyPage';
 import Footer from 'components/footer/Footer';
 
 const App = () => {
-  
   return (
     <>
       <Header />
       <Main>
         <Routes>
-          <Route path="/myPage" element={<MyPage />} />
+          <Route path="/MyPage" element={<MyPage />} />
         </Routes>
       </Main>
       <Footer />
