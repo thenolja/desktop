d9687260361777d7839b2da0d439c141aabae86c
hann3 <soyo_0131@naver.com>
2022-03-17 12:54:52 +0900

    ✨ feat: prevent unauthorized access
    - 로그인을 하지 않거나 로그아웃을 한 사용자가 마이페이지에 접근하는 것을 방지

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,10 +1,14 @@
 import ReservationList from 'components/Reservation/Reservation';
 import UserInfo from 'components/UserInfo/UserInfo';
 import { useMemo, useState } from 'react';
+import { useAppSelector } from 'src/contexts/state.type';
+import { selectAuth } from 'src/contexts/auth';
 import SignOut from './MyPage.style';
+import ProtectedRoute from '../ProtectedRouter/ProtectedRouter';
 
 const MyPage = () => {
   const [isEditing, setEditingMode] = useState(false);
+  const { currentUser, email } = useAppSelector(selectAuth);
 
   const MemoizedUserInfo = useMemo(
     () => <UserInfo isEditing={isEditing} setEditingMode={setEditingMode} />,
@@ -14,11 +18,11 @@ const MyPage = () => {
   const MemoizedSignOut = useMemo(() => <SignOut>{isEditing ? <button>회원탈퇴</button> : ''}</SignOut>, [isEditing]);
 
   return (
-    <>
+    <ProtectedRoute isAllow={!!(currentUser && email)}>
       {MemoizedUserInfo}
       {MemoizedReservationList}
       {MemoizedSignOut}
-    </>
+    </ProtectedRoute>
   );
 };
 
