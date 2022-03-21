743c8f47d4e758e46684179c54607163dbd73e82
ParkSangEon <sangeon1500@daum.net>
2022-03-21 21:59:56 +0900

    feat : set dateformat when submit query in SearchForm Component

--- frontend/src/components/Index/SearchForm/Input/PersoInput.tsx
+++ frontend/src/components/Index/SearchForm/Input/PersoInput.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import { StyledDiv } from './InputDiv.style';
 
-const PersoInput = () => {
+const PersoInput = ({ setPerson }) => {
+  const handleChange = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setPerson(target.value);
+  };
+
   return (
     <StyledDiv width="267px">
       <img src="/src/assets/person.png" alt="" />
-      <input id="person" placeholder="인원 수" />
+      <input id="person" placeholder="인원 수" type="number" onChange={handleChange} />
     </StyledDiv>
   );
 };
