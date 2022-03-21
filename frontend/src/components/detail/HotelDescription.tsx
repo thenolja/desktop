9ec564334c4323926c9d628b41ef14d8098fd5c0
sians5544 <tocic@naver.com>
2022-03-21 13:47:03 +0900

    refactor: hotel Description style refactor

--- frontend/src/components/detail/HotelDescription.tsx
+++ frontend/src/components/detail/HotelDescription.tsx
@@ -6,45 +6,27 @@ import {
   Grade,
   Allbutton,
   GradeText,
-  AmenitiesBox,
-  AmenitiesTitle,
-  AmenitiesUL,
-  AmenitiesList,
+  TagBox,
+  TagTitle,
 } from './HotelDescription.style';
 
 const HotelDescription = ({ ...restProps }) => {
   return (
     <>
       <Description>
-        <Title>소노탐 고양(구 엠블호텔 고양)</Title>
-        <SubTitle>Sono Calm Goyang (formerly The MVL Goyang)</SubTitle>
+        <TitleWrapper>
+          <Title>소노탐 고양(구 엠블호텔 고양)</Title>
+          <SubTitle>Sono Calm Goyang (formerly The MVL Goyang)</SubTitle>
+        </TitleWrapper>
         <TitleWrapper>
           <Grade>4.5/5-훌륭해요</Grade>
           <GradeText>이 숙박 시설의 청격 상태에 대한 고객님들의 평점은 4.5/5 입니다</GradeText>
           <Allbutton>871개 이용후기 모두보기</Allbutton>
         </TitleWrapper>
-        <AmenitiesBox>
-          <AmenitiesTitle>인기 편의 시설</AmenitiesTitle>
-          <AmenitiesUL>
-            <AmenitiesList>세탁 서비스</AmenitiesList>
-            <AmenitiesList>주차 포함</AmenitiesList>
-            <AmenitiesList>무료 WiFi</AmenitiesList>
-            <AmenitiesList>에어컨</AmenitiesList>
-            <AmenitiesList>수영장</AmenitiesList>
-            <AmenitiesList>레스토랑</AmenitiesList>
-          </AmenitiesUL>
-          <Allbutton>모두보기</Allbutton>
-        </AmenitiesBox>
-        <AmenitiesBox>
-          <AmenitiesTitle>위생 및 안전 조치</AmenitiesTitle>
-          <AmenitiesUL>
-            <AmenitiesList>소독제로 청소함</AmenitiesList>
-            <AmenitiesList>개인 보호 장비</AmenitiesList>
-            <AmenitiesList>손 소독제 제공됨</AmenitiesList>
-            <AmenitiesList>사회적 거리두기</AmenitiesList>
-          </AmenitiesUL>
-          <Allbutton>모두보기</Allbutton>
-        </AmenitiesBox>
+        <TagBox>
+          <TagTitle>숙소소개</TagTitle>
+          <p>4성급 호텔, 1 개 레스토랑 있음, 컨벤션 센터와 연결, 일산 호수 공원 근처</p>
+        </TagBox>
       </Description>
     </>
   );
