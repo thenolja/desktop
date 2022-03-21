40ddac214947585e1238d08695b44a69c8f055a7
ktmihs <yellowgreen423@gmail.com>
2022-03-22 01:29:24 +0900

    feat: get number of hotel reviews - create a get api requesting the number of hotel reivews

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -159,4 +159,39 @@ const getAllRoomList = async (hotelId: string): Promise<[]> => {
     });
 };
 
-export { getAllHotelList, getNearHotelList, getLocalHotelList, getAllRoomList };
+const getReviewTitleData = async (hotelId: string): Promise<[]> => {
+  const options = {
+    method: 'GET',
+    url: 'https://hotels4.p.rapidapi.com/properties/get-details',
+    params: {
+      id: hotelId,
+      currency: 'KRW',
+      locale: 'ko_KR'
+    },
+    headers: {
+      'x-rapidapi-host': 'hotels4.p.rapidapi.com',
+      'x-rapidapi-key': '65c2b8e40fmshf6785c8d21db1cbp1581c4jsnae60b44b1c91'
+    }
+  };
+
+  return await axios.request(options)
+    .then(
+      ({
+        data: {
+          data: {
+            body: {
+               guestReviews:{
+                 brands
+               }
+            },
+          },
+        },
+      }) => brands,
+    )
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+
+export { getAllHotelList, getNearHotelList, getLocalHotelList, getAllRoomList, getReviewTitleData };
