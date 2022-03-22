5597bdaaba6f748ce76eb1597d4bfe6aafb2ae91
sians5544 <tocic@naver.com>
2022-03-22 23:44:05 +0900

    #25 feat: hotel info api

--- frontend/src/components/detail/HotelDescription.tsx
+++ frontend/src/components/detail/HotelDescription.tsx
@@ -10,22 +10,30 @@ import {
   TagTitle,
 } from './HotelDescription.style';
 
-const HotelDescription = ({ ...restProps }) => {
+const HotelDescription = ({ hotelInfo }) => {
   return (
     <>
       <Description>
         <TitleWrapper>
-          <Title>소노탐 고양(구 엠블호텔 고양)</Title>
-          <SubTitle>Sono Calm Goyang (formerly The MVL Goyang)</SubTitle>
+          <Title>{hotelInfo.name}</Title>
         </TitleWrapper>
         <TitleWrapper>
-          <Grade>4.5/5-훌륭해요</Grade>
-          <GradeText>이 숙박 시설의 청격 상태에 대한 고객님들의 평점은 4.5/5 입니다</GradeText>
-          <Allbutton>871개 이용후기 모두보기</Allbutton>
+          <Grade>
+            {' '}
+            {hotelInfo.formattedRating}/{hotelInfo.formattedScale}-{hotelInfo.badgeText}
+          </Grade>
+          <GradeText>
+            이 숙박 시설의 청격 상태에 대한 고객님들의 평점은 {hotelInfo.formattedRating}/ {hotelInfo.formattedScale}{' '}
+            입니다
+          </GradeText>
+          <Allbutton>{hotelInfo.totalcnt}개 이용후기 모두보기</Allbutton>
         </TitleWrapper>
         <TagBox>
           <TagTitle>숙소소개</TagTitle>
-          <p>4성급 호텔, 1 개 레스토랑 있음, 컨벤션 센터와 연결, 일산 호수 공원 근처</p>
+          {hotelInfo.tagline}
+          <p>- {hotelInfo.hotelSize} </p>
+          <p>- {hotelInfo.arriving}</p>
+          <p>- {hotelInfo.leaving}</p>
         </TagBox>
       </Description>
     </>
