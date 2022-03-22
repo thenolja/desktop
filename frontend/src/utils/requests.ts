823a1dc626edad09200e1ff83c77caf9bd97a8b9
ktmihs <yellowgreen423@gmail.com>
2022-03-22 14:51:27 +0900

    wip: pause a request pause the request to limit the number of api requests

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -135,7 +135,7 @@ const getAllRoomList = async (hotelId: string): Promise<[]> => {
     },
     headers: {
       'x-rapidapi-host': 'hotels4.p.rapidapi.com',
-      'x-rapidapi-key': '65c2b8e40fmshf6785c8d21db1cbp1581c4jsnae60b44b1c91'
+      // 'x-rapidapi-key': '65c2b8e40fmshf6785c8d21db1cbp1581c4jsnae60b44b1c91'
     }
   };
 
@@ -170,7 +170,7 @@ const getReviewTitleData = async (hotelId: string): Promise<[]> => {
     },
     headers: {
       'x-rapidapi-host': 'hotels4.p.rapidapi.com',
-      'x-rapidapi-key': '65c2b8e40fmshf6785c8d21db1cbp1581c4jsnae60b44b1c91'
+      // 'x-rapidapi-key': '65c2b8e40fmshf6785c8d21db1cbp1581c4jsnae60b44b1c91'
     }
   };
 
