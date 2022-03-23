6e43e5bb14928ee11e9f24dc2fdc59951f04af2c
ktmihs <yellowgreen423@gmail.com>
2022-03-23 15:52:43 +0900

    feat: get check-in check-out date get changed check-in and check-out request data on the changed date

--- frontend/src/pages/Detail/Rooms.tsx
+++ frontend/src/pages/Detail/Rooms.tsx
@@ -3,27 +3,40 @@ import Room from "components/Room/Room";
 import { useEffect, useState } from "react";
 import { getAllRoomList } from "src/utils/requests";
 import {Buttons, SelectBtn} from "./Rooms.style";
+import { addDays } from 'date-fns';
 
 const Rooms = () => {
 
   const [hotelId, setHotelId] = useState<string>('229056');
   const [rooms, setRooms]=useState<object[]>([]);
 
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [endDate, setEndDate] = useState<Date>(addDays(new Date(), 1));
+  
   useEffect(()=>{
     const requestRooms = async () => {
-      const Rooms = await getAllRoomList(hotelId);
+      const checkIn=`${startDate.getFullYear()}-${startDate.getMonth()+1}-${startDate.getDate()}`;
+      const checkOut=`${endDate.getFullYear()}-${endDate.getMonth()+1}-${endDate.getDate()}`;
+      
+      const Rooms = await getAllRoomList(hotelId, checkIn, checkOut);
       setRooms(Rooms);
+      console.log(Rooms)
     };
     requestRooms();
-  },[]);
+  },[endDate]);
 
   return(
     <div>
-      <CheckInOut checkIn={new Date()} checkOut={new Date()} />
+      <CheckInOut
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+      />
       <ul>
-        {/* {rooms.map((room, index) => 
+        {rooms.map((room, index) => 
           <Room key={index} room={room} />
-        )} */}
+        )}
       </ul>
       <Buttons>
         <SelectBtn>예약하기</SelectBtn>
