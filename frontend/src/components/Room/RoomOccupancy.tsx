f7ffc1cada6236b87547cc237e195551da1f8730
ktmihs <yellowgreen423@gmail.com>
2022-03-26 23:25:47 +0900

    refactor: divide a file into components - RoomInfo.tsx - RoomOccupancy.tsx - RoomPriceInfo.tsx - DetailRoomInfo.tsx

---
+++ frontend/src/components/Room/RoomOccupancy.tsx
@@ -0,0 +1,14 @@
+import { OccupancyType } from "./Room.types"
+import { People } from "./Room.style"
+
+export const RoomOccupancy = ({maxOccupancy}:OccupancyType) => {
+  return(
+    <>
+      {maxOccupancy && (
+        <People>
+          기준 {maxOccupancy.total}명 / 최대 {maxOccupancy.children + maxOccupancy.total}명
+        </People>
+      )}
+    </>
+  )
+}
