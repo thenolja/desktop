a1effb87dac93e8926b1e924773cdde5a2debe72
hann3 <soyo_0131@naver.com>
2022-03-29 13:39:52 +0900

    🔀 Merge: review type change

--- frontend/src/components/Review/Review.type.ts
+++ frontend/src/components/Review/Review.type.ts
@@ -1,25 +1,26 @@
-export interface ReviewProps{
+export interface ReviewProps {
   id?: number;
   star?: number;
+  reservationId?: number;
   writeTime?: string;
   nickname?: string;
   spec?: string;
   reviewText?: string;
 }
 
-export interface ReviewType{
-  review:{
-    itineraryId?:string;
-    rating?:number;
-    reviewDate?:string;
-    reviewer?:{
-      name?:string;
-    }
-    tripTypeText?:string;
-    description?:string;
-  }
+export interface ReviewType {
+  review: {
+    itineraryId?: string;
+    rating?: number;
+    reviewDate?: string;
+    reviewer?: {
+      name?: string;
+    };
+    tripTypeText?: string;
+    description?: string;
+  };
 }
 
-export interface ReviewsType{
+export interface ReviewsType {
   reviews: object[];
 }
