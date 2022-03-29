4f50be77a93b000dfbf6926c939e2067dc60a967
ktmihs <yellowgreen423@gmail.com>
2022-03-29 22:40:40 +0900

    refactor: divide component divide the reservation page into large components

---
+++ frontend/src/components/Payment/PriceInfo.tsx
@@ -0,0 +1,21 @@
+import { SectionTitle, FormArticle, TotalWrapper } from './Payment.style';
+
+const PriceInfo = ({ cost }) => {
+  return (
+    <FormArticle>
+      <SectionTitle>금액 및 할인 정보</SectionTitle>
+      <section>
+        <TotalWrapper>
+          <span>총 예약 금액</span>
+          <span style={{ fontWeight: 700 }}>{cost}원</span>
+        </TotalWrapper>
+        <TotalWrapper style={{ fontWeight: 700 }}>
+          <span>결제 금액</span>
+          <span style={{ color: '#de2e5f', fontSize: '18px' }}>{cost}원</span>
+        </TotalWrapper>
+      </section>
+    </FormArticle>
+  )
+}
+
+export default PriceInfo;
---
+++ frontend/src/components/Payment/Visiting.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from "react";
+import { FormArticle, Necessary, SectionBody, SectionTitle } from "./Payment.style";
+
+
+const Visiting = ({ reservation, setReservation }) => {
+
+  const carRef = useRef();
+
+  // 숙소 방문 수단을 기본적으로 차량으로 세팅
+  useEffect(() => {
+    carRef.current.checked = reservation.hasCar;
+  }, []);
+
+  const handleVisited = () => {
+    setReservation({
+      ...reservation,
+      hasCar: carRef.current.checked
+    })
+  }
+
+  return (
+    <FormArticle>
+      <SectionTitle>
+        숙소 방문 수단<Necessary>*</Necessary>
+      </SectionTitle>
+      <SectionBody>
+        <div className="visited">
+          <input type="radio" ref={carRef} name="visited" id="car" onChange={handleVisited} />
+          <label htmlFor="car">차량</label>
+        </div>
+        <div className="visited">
+          <input type="radio" name="visited" id="work" onChange={handleVisited} />
+          <label htmlFor="work">도보</label>
+        </div>
+      </SectionBody>
+    </FormArticle>
+  )
+}
+
+export default Visiting;
