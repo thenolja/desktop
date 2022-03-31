574232b5d8ff5ecc0b0707401d49ea0cd41e248c
ktmihs <yellowgreen423@gmail.com>
2022-04-01 02:09:00 +0900

    refactor: modify review type combine review type interfaces into one

--- frontend/src/components/Review/Review.type.ts
+++ frontend/src/components/Review/Review.type.ts
@@ -1,29 +1,32 @@
-export interface ReviewProps {
-  review: {
-    id: number;
-    hotelId: string;
-    reservationId: number;
-    star: number;
-    writeTime: string;
-    nickname: string;
-    spec: string;
-    reviewText: string;
-  };
-}
+import { MouseEventHandler } from "react";
 
-export interface ReviewType {
+export interface ReviewType{
   review: {
-    itineraryId: string;
-    rating: number;
-    reviewDate: string;
+    id?: number;
+    hotelId?: string;
+    reservationId?: number;
+    star?: number;
+    writeTime?: string;
+    nickname?: string;
+    spec?: string;
+    reviewText?: string;
+
+    itineraryId?: string;
+    rating?: number;
+    reviewDate?: string;
     reviewer?: {
       name: string;
     };
-    tripTypeText: string;
-    description: string;
+    tripTypeText?: string;
+    description?: string;
   };
+}
+
+export interface ReviewProps extends ReviewType{
+  handleDelete?: MouseEventHandler<HTMLButtonElement>
 }
 
 export interface ReviewsType {
   reviews: object[];
+  handleDelete?: MouseEventHandler<HTMLButtonElement>
 }
