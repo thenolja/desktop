0cdc5be22ff208ae5d701667be2ae903c9d7a24f
ktmihs <yellowgreen423@gmail.com>
2022-04-25 01:27:45 +0900

    feat: apply swr in Rooms component retrieve stored data using swr

--- frontend/src/pages/Detail/Rooms.tsx
+++ frontend/src/pages/Detail/Rooms.tsx
@@ -2,6 +2,8 @@ import QueryString, { ParsedQs } from 'qs';
 import { memo, useCallback, useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { addDays } from 'date-fns';
+import useSWR, { SWRConfig } from 'swr';
+
 import Spinner from 'components/Spinner/Spinner';
 import changeDateFormatToIsoSTring from '../../utils/dateToISOString';
 
@@ -19,9 +21,7 @@ const Rooms = () => {
   const location = useLocation();
   const { checkIn, checkOut }: ParsedQs = QueryString.parse(location.search, { ignoreQueryPrefix: true });
 
-  const [rooms, setRooms] = useState<DetailRoomProps[] | undefined>([]);
   const [selectedRoom, setSelectedRoom] = useState<RoomProps>();
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   const [startDate, setStartDate] = useState<Date>(new Date());
   const [endDate, setEndDate] = useState<Date>(addDays(new Date(), 1));
@@ -40,29 +40,30 @@ const Rooms = () => {
     window.sessionStorage.setItem('SELECTED_ROOM', JSON.stringify({ ...selectedRoom, startDate, endDate }));
   }, [selectedRoom]);
 
-  useEffect(() => {
-    const requestRooms = async () => {
-      setRooms([]);
-      setIsLoaded(true);
-      const checkIn = changeDateFormatToIsoSTring(startDate);
-      const checkOut = changeDateFormatToIsoSTring(endDate);
+  
+  const requestRooms = async () => {
+    const checkIn = changeDateFormatToIsoSTring(startDate);
+    const checkOut = changeDateFormatToIsoSTring(endDate);
 
-      const Rooms = await getAllRoomList(hotelId, checkIn, checkOut);
-      const reservedRooms = await getReservedRooms(hotelId, checkIn, checkOut);
-
-      const nonReservedRooms = reservedRooms.length && Rooms && Rooms.length ? Rooms.filter((room: RoomProps) => reservedRooms.indexOf(room.name) === -1) : Rooms;
-      Rooms ? setRooms(nonReservedRooms) : setRooms([]);
-      setIsLoaded(false);
-    };
-    requestRooms();
-    setSelectedRoom({});
-  }, [endDate]);
-
+    const rooms = await getAllRoomList(hotelId, checkIn, checkOut);
+    const reservedRooms = await getReservedRooms(hotelId, checkIn, checkOut);
+    
+    const nonReservedRooms = reservedRooms.length && rooms && rooms.length ? rooms.filter((room: RoomProps) => reservedRooms.indexOf(room.name) === -1) : rooms;
+    return rooms ? nonReservedRooms : [];
+  };
+  
+  function fetcher() {
+    return new Promise(resolve => {
+      resolve(requestRooms());
+    });
+  }
+  const {data:rooms, error}=useSWR(changeDateFormatToIsoSTring(startDate)+changeDateFormatToIsoSTring(endDate),fetcher);
+  
   return (
-    <>
+    <SWRConfig value={{ provider: cache => cache }}>
       <CheckInOut startDate={startDate} setCheckInDate={setCheckInDate} endDate={endDate} setCheckOutDate={setCheckOutDate} />
-      {isLoaded && <Spinner />}
-      {rooms.length ?
+      {(!rooms && !error) && <Spinner />}
+      {rooms && rooms.length ?
         <>
           <ul>
             {rooms.map((room, index) => <Room key={index} room={room} setSelector={setSelector} />)}
@@ -70,13 +71,13 @@ const Rooms = () => {
           <SelectBar selectedRoom={selectedRoom} hotelId={hotelId} setSessionStorage={setSessionStorage} />
         </>
         :
-        !isLoaded &&
+        rooms && !rooms.length &&
         <NotFoundRooms>
           <p>예약할 수 있는 객실이 존재하지 않습니다.</p>
           <p>다른 날짜를 선택해주세요.</p>
         </NotFoundRooms>
       }
-    </>
+    </SWRConfig>
   );
 };
 
