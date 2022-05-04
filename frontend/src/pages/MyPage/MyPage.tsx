4f9ab4b26dda012801a222efcd8e5756b40f4fee
hann3 <soyo_0131@naver.com>
2022-05-04 21:51:38 +0900

    ♻️ Refactor: state 이름 변경 및 정규식 분리
    - 프로필 편집 상태를 나타내는 state명을 변경
    - 가독성을 위해 닉네임, 패스워드 관련 정규식을 따로 분리

--- frontend/src/pages/MyPage/MyPage.tsx
+++ frontend/src/pages/MyPage/MyPage.tsx
@@ -6,16 +6,16 @@ import SignOut from './MyPage.style';
 import Profile from 'components/UserInfo/Profile';
 
 const MyPage = () => {
-  const [isEditing, setEditingMode] = useState<boolean>(false);
+  const [isEditingMode, setEditingMode] = useState<boolean>(false);
 
   return (
     <>
       <Helmet>
         <title>{setDocumentTitle('마이페이지')}</title>
       </Helmet>
-      <Profile isEditing={isEditing} setEditingMode={setEditingMode} />
+      <Profile isEditingMode={isEditingMode} setEditingMode={setEditingMode} />
       <ReservationList />
-      <SignOut>{isEditing ? <button className="submit">회원탈퇴</button> : ''}</SignOut>,
+      <SignOut>{isEditingMode ? <button className="submit">회원탈퇴</button> : ''}</SignOut>,
     </>
   );
 };
