ddfbb41356639c4e41f375a5250dee14b7293c90
sians5544 <tocic@naver.com>
2022-05-13 14:10:33 +0900

    docs: delete file

--- frontend/src/utils/carts.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const getUserCart = async (id: string) => {
-  return await axios
-    .get(`/api/cart/${id}`)
-    .then(({ data }) => data)
-    .catch(e => console.log(e));
-};
