56d50a1eb3005262a62d16fb5a13c12c5c372872
sians5544 <tocic@naver.com>
2022-05-24 23:40:39 +0900

    feat: Add room shopping cart function

--- frontend/src/components/Room/Selector/Seletor.type.ts
+++ frontend/src/components/Room/Selector/Seletor.type.ts
@@ -1,7 +1,9 @@
-import { RoomProps } from "../Room.types";
+import { RoomProps } from '../Room.types';
 
-export interface SelectorType{
+export interface SelectorType {
+  startDate?: string;
+  endDate?: string;
   selectedRoom: RoomProps;
   hotelId: string;
-  setSessionStorage: ()=> void;
+  setSessionStorage: () => void;
 }
