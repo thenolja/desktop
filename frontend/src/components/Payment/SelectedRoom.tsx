f0f263d6eca4b2ee8b70c0ca2f44665e9c0f0b12
ktmihs <yellowgreen423@gmail.com>
2022-05-29 14:55:55 +0900

    feat: add key to SelectedRoomItem component add key attribute

--- frontend/src/components/Payment/SelectedRoom.tsx
+++ frontend/src/components/Payment/SelectedRoom.tsx
@@ -7,7 +7,7 @@ const SelectedRoom=({data})=>{
   return(
     <FormArticle>
       <SectionTitle>선택한 객실 정보</SectionTitle>
-      {data.map(selectedRoom =><SelectedRoomItem selectedRoom={selectedRoom} /> )}
+      {data.map(selectedRoom =><SelectedRoomItem key={selectedRoom.name} selectedRoom={selectedRoom} /> )}
     </FormArticle>
   )
 }
