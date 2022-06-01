353043823b10e2659204f5ed651e40fd06ec9fef
sians5544 <tocic@naver.com>
2022-06-01 18:39:59 +0900

    edit: change selectRoom output type

--- frontend/src/components/Payment/SelectedRoomItem.tsx
+++ frontend/src/components/Payment/SelectedRoomItem.tsx
@@ -1,23 +1,25 @@
-import { memo } from "react";
-import { SelectedRoomType } from "./Payment.type";
-import { SelectedBody, SelectedInfos, SelectedInfo } from "./Payment.style";
+import { memo } from 'react';
+import { SelectedRoomType } from './Payment.type';
+import { SelectedBody, SelectedInfos, SelectedInfo } from './Payment.style';
 
-const SelectedRoomItem=({selectedRoom}:SelectedRoomType)=>{
-  return(
+const SelectedRoomItem = ({ selectedRoom }: SelectedRoomType) => {
+  return (
     <SelectedBody>
       <img src={selectedRoom.photo} alt={selectedRoom.name} />
       <SelectedInfos>
         <SelectedInfo>
           <p className="hotelName">{selectedRoom.hotelName}</p>
           <p>[ {selectedRoom.name} ]</p>
-          <p>{selectedRoom.checkIn} - {selectedRoom.checkOut}</p>
+          <p>
+            {selectedRoom.checkIn} - {selectedRoom.checkOut}
+          </p>
         </SelectedInfo>
         <SelectedInfo>
-          <p className="eachCost">{selectedRoom.cost.toLocaleString()}원</p>
+          <p className="eachCost">{Number(selectedRoom.cost).toLocaleString('ko-KR')}원</p>
         </SelectedInfo>
       </SelectedInfos>
     </SelectedBody>
-  )
-}
+  );
+};
 
 export default memo(SelectedRoomItem);
